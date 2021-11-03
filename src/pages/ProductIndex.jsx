b4b3d2fbe71655f8214b742cd5584f8fdbd0b568
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-03 11:48:56 +0800

    initial idea

--- src/pages/ProductIndex.jsx
+++ src/pages/ProductIndex.jsx
@@ -1,13 +1,128 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 const ProductIndex = () => (
   <div id="product-index" className="container">
-    <header className="text-center border-bottom">
-      <h1>Product Index</h1>
-      <div><Link to="/">Home Page</Link></div>
-      <div><Link to="/ProductShow">Product Show</Link></div>
+    <header className="text-center mb-3">
+      <h1>Category.catName</h1>
     </header>
+
+    <CardGroup>
+      <Card onClick>
+        <Card.Img variant="top" src="https://rukminim1.flixcart.com/image/714/857/kjbr8280-0/t-shirt/a/p/1/m-tsrt-118-reya-original-imafyx4vqsmyyduq.jpeg?q=50" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="https://imgs.michaels.com/MAM/assets/1/726D45CA1C364650A39CD1B336F03305/img/893E44B4248847338CD88E85BD79D361/10186027_r.jpg?fit=inside|140:140,https://imgs.michaels.com/MAM/assets/1/726D45CA1C364650A39CD1B336F03305/img/893E44B4248847338CD88E85BD79D361/10186027_r.jpg?fit=inside|220:220,https://imgs.michaels.com/MAM/assets/1/726D45CA1C364650A39CD1B336F03305/img/893E44B4248847338CD88E85BD79D361/10186027_r.jpg?fit=inside|540:540" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="https://4.imimg.com/data4/AA/HC/MY-26596027/men-s-fancy-t-shirt-500x500.jpg" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="https://m.media-amazon.com/images/I/61MSZWTT3IL._UY550_.jpg" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardGroup>
+    <CardGroup>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardGroup>
+    <CardGroup>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card onClick>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Product.productName</Card.Title>
+          <Card.Text>
+            Price = $Product.price
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardGroup>
+
   </div>
 )
 
