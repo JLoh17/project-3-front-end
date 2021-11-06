a172f1b4c18c1da6af75af887cdf1ca2e2d7df3d
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-06 21:39:20 +0800

    Changed CartTable component from function to class-base

--- src/components/CartTable.jsx
+++ src/components/CartTable.jsx
@@ -8,119 +8,134 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-const CartTable = () => (
-  <div id="ComponentsCartTable" className="container">
+class CartTable extends React.Component {
+  constructor(props) {
+    super(props)
 
-    <Table className="text-center mt-3" responsive="md">
-      <thead>
-        <tr>
-          <th colSpan="2" />
-          <th>Unit Cost</th>
-          <th>Quantity</th>
-          <th>Subtotal</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <Image src="https://i.pinimg.com/736x/4a/91/a6/4a91a6b4f09c7df99a6352db06f7a701.jpg" className="pic-resize" />
-          </td>
-          <td>ProductName</td>
-          <td>$700</td>
-          <td>
-            <Form>
-              <Form.Control as="select" aria-label="quantity" name="quantity">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </Form.Control>
-            </Form>
-          </td>
-          <td>Subtotal</td>
-          <td>
-            <div className="fas fa-trash-alt trashBtn"> Remove</div>
-          </td>
-        </tr>
+    this.state = {
 
-      </tbody>
+    }
+  }
 
-      <tbody>
-        <tr>
-          <td colSpan="3" />
-          <td>
-            <h4>Total:</h4>
-          </td>
-          <td>
-            <h4>$100.00</h4>
-          </td>
-          <td>
-            <Button variant="success"> Next Step </Button>
-          </td>
-        </tr>
-      </tbody>
+  render() {
+    return (
+      <div id="ComponentsCartTable" className="container">
 
-    </Table>
-  </div>
+        <Table className="text-center mt-3" responsive="md">
+          <thead>
+            <tr>
+              <th colSpan="2" />
+              <th>Unit Cost</th>
+              <th>Quantity</th>
+              <th>Subtotal</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
 
-  // Grid version
-  // <div id="ComponentsCartTable" className="container">
+            {/* Map this section */}
+            <tr>
+              <td>
+                <Image src="https://i.pinimg.com/736x/4a/91/a6/4a91a6b4f09c7df99a6352db06f7a701.jpg" className="pic-resize" />
+              </td>
+              <td>ProductName</td>
+              <td>$700</td>
+              <td>
+                <Form>
+                  <Form.Control as="select" aria-label="quantity" name="quantity">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Form.Control>
+                </Form>
+              </td>
+              <td>Subtotal</td>
+              <td>
+                <div className="fas fa-trash-alt trashBtn"> Remove</div>
+              </td>
+            </tr>
 
-  //   <Container className="mt-3 text-center">
-  //     <Row>
-  //       <Col />
-  //       <Col />
-  //       <Col>
-  //         <h4>Unit Cost</h4>
-  //       </Col>
-  //       <Col>
-  //         <h4>Quantity</h4>
-  //       </Col>
-  //       <Col>
-  //         <h4>Subtotal</h4>
-  //       </Col>
-  //       <Col />
-  //     </Row>
-  //     <Row className="d-flex align-items-center my-2">
-  //       <Col>
-  //         <Image src="https://i.pinimg.com/736x/4a/91/a6/4a91a6b4f09c7df99a6352db06f7a701.jpg" className="pic-resize" />
-  //       </Col>
-  //       <Col>ProductName</Col>
-  //       <Col>$700</Col>
-  //       <Col>
-  //         <Form>
-  //           <Form.Control as="select" aria-label="quantity" name="quantity">
-  //             <option value="1">1</option>
-  //             <option value="2">2</option>
-  //             <option value="3">3</option>
-  //             <option value="4">4</option>
-  //             <option value="5">5</option>
-  //           </Form.Control>
-  //         </Form>
-  //       </Col>
-  //       <Col>Subtotal</Col>
-  //       <Col>
-  //         <div className="fas fa-trash-alt trashBtn"> Remove</div>
-  //       </Col>
-  //     </Row>
-  //     <Row>
-  //       <Col />
-  //       <Col />
-  //       <Col />
-  //       <Col>
-  //         <h4>Total:</h4>
-  //       </Col>
-  //       <Col>
-  //         <h4>$100.00</h4>
-  //       </Col>
-  //       <Col />
+          </tbody>
 
-  //     </Row>
-  //   </Container>
+          <tbody>
+            <tr>
+              <td colSpan="3" />
+              <td>
+                <h4>Total:</h4>
+              </td>
+              <td>
+                <h4>$100.00</h4>
+              </td>
+              <td>
+                <Button variant="success"> Next Step </Button>
+              </td>
+            </tr>
+          </tbody>
 
-  // </div>
-)
+        </Table>
+      </div>
+
+    )
+  }
+}
 
 export default CartTable
+
+// Grid version
+// <div id="ComponentsCartTable" className="container">
+
+//   <Container className="mt-3 text-center">
+//     <Row>
+//       <Col />
+//       <Col />
+//       <Col>
+//         <h4>Unit Cost</h4>
+//       </Col>
+//       <Col>
+//         <h4>Quantity</h4>
+//       </Col>
+//       <Col>
+//         <h4>Subtotal</h4>
+//       </Col>
+//       <Col />
+//     </Row>
+//     <Row className="d-flex align-items-center my-2">
+//       <Col>
+//         <Image src="https://i.pinimg.com/736x/4a/91/a6/4a91a6b4f09c7df99a6352db06f7a701.jpg" className="pic-resize" />
+//       </Col>
+//       <Col>ProductName</Col>
+//       <Col>$700</Col>
+//       <Col>
+//         <Form>
+//           <Form.Control as="select" aria-label="quantity" name="quantity">
+//             <option value="1">1</option>
+//             <option value="2">2</option>
+//             <option value="3">3</option>
+//             <option value="4">4</option>
+//             <option value="5">5</option>
+//           </Form.Control>
+//         </Form>
+//       </Col>
+//       <Col>Subtotal</Col>
+//       <Col>
+//         <div className="fas fa-trash-alt trashBtn"> Remove</div>
+//       </Col>
+//     </Row>
+//     <Row>
+//       <Col />
+//       <Col />
+//       <Col />
+//       <Col>
+//         <h4>Total:</h4>
+//       </Col>
+//       <Col>
+//         <h4>$100.00</h4>
+//       </Col>
+//       <Col />
+
+//     </Row>
+//   </Container>
+
+// </div>
