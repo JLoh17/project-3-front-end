86cde86ba5f216eadb38e7f4fd8f35f747d74bff
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-06 18:31:01 +0800

    myCart rough design

---
+++ src/components/CartTable.jsx
@@ -0,0 +1,61 @@
+// Components - Cart Table
+
+import React from 'react'
+import Table from 'react-bootstrap/Table'
+import Image from 'react-bootstrap/Image'
+import Form from 'react-bootstrap/Form'
+
+const CartTable = () => (
+  <div id="ComponentsCartTable" className="container">
+
+    <Table className="text-center mt-3">
+      <thead>
+        <tr>
+          <th colSpan="2" />
+          <th>Unit Cost</th>
+          <th>Quantity</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <Image src="https://modone.com/eng_pl_Mens-shoes-T326-brown-14029_7.jpg" className="pic-resize" />
+          </td>
+          <td>ProductName</td>
+          <td>$700</td>
+          <td>
+            <Form>
+              <Form.Control as="select" aria-label="quantity" name="quantity">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </Form.Control>
+            </Form>
+          </td>
+          <td>Subtotal</td>
+          <td>
+            <div className="fas fa-trash-alt trashBtn"> Remove</div>
+          </td>
+        </tr>
+      </tbody>
+
+      <tbody>
+        <tr>
+          <td colSpan="3" />
+          <td>
+            <h4>Total:</h4>
+          </td>
+          <td>
+            <h4>$100.00</h4>
+          </td>
+        </tr>
+      </tbody>
+
+    </Table>
+  </div>
+)
+
+export default CartTable
--- src/pages/my/Cart.jsx
+++ src/pages/my/Cart.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import CartTable from '@/components/CartTable'
 
 const MyCart = () => (
   <div id="my-cart" className="container">
     <header className="text-center border-bottom">
-      <h1>MY CART</h1>
-      <div>Your shopping cart is currently empty.</div>
-      <div><Link to="/products">View Products</Link></div>
+      <h1>MY CART </h1>
+      <div> Your shopping cart is currently empty.</div>
+      <div> <Link to="/products"> View Products </Link></div>
     </header>
+
+    <CartTable />
+
+    <div className="d-flex justify-content-end">
+      <Button variant="success"> Next Step </Button>
+    </div>
+
   </div>
 )
 
