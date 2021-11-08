010b3ae9af24abcc3e01fac890d1e2dfd55e1842
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-08 18:03:52 +0800

    table amended

--- src/pages/my/orders/index.jsx
+++ src/pages/my/orders/index.jsx
@@ -16,10 +16,17 @@ class MyOrdersIndex extends React.Component {
       sort: 'createdAt',
       status: ''
     }
+
+    this.orderShow = this.orderShow.bind(this)
   }
 
   componentDidMount() {
     this.props.getOrdersIndex(this.state)
+  }
+
+  orderShow(orderId) {
+    const { history: { push } } = this.props
+    push(`/my/orders/${orderId}`)
   }
 
   render() {
@@ -46,22 +53,25 @@ class MyOrdersIndex extends React.Component {
           <tbody>
             {
             listOrder.map((order) => (
-              <tr>
-                <td>{order.createdAt.slice(0, 10)}</td>
-                <td>{order.id}</td>
-                <td>{order.grandTotal}</td>
-                <td>{order.status}</td>
+              <tr className="cursor-icon">
+                <td onClick={() => this.orderShow(order.id)}>{order.createdAt.slice(0, 10)}</td>
+                <td onClick={() => this.orderShow(order.id)}>
+                  {order.id}</td>
+                <td onClick={() => this.orderShow(order.id)}>{order.grandTotal}</td>
+                <td onClick={() => this.orderShow(order.id)}>{order.status}</td>
+                {/* pointEvent stops this from being click under the table row */}
+                {/* Remove this and put it in style */}
                 <td>
                   {
-                  order.status === 'Pending Payment' ? (
-                    <>
-                      <span className="fas fa-trash-alt">Cancel</span>
-                      <span className="fas fa-clone">Duplicate Order</span>
-                    </>
-                  ) : (
-                    <span className="fas fa-clone">Duplicate Order</span>
-                  )
-                }
+                    order.status === 'Pending Payment' ? (
+                      <>
+                        <span className="click-auto fas fa-trash-alt" onClick>Cancel</span>
+                        <span className="click-auto fas fa-clone" onClick>Duplicate Order</span>
+                      </>
+                    ) : (
+                      <span className="click-auto fas fa-clone" onClick>Duplicate Order</span>
+                    )
+                  }
                 </td>
               </tr>
             ))
@@ -77,7 +87,9 @@ class MyOrdersIndex extends React.Component {
 
 MyOrdersIndex.propTypes = {
   getOrdersIndex: PropTypes.func.isRequired,
-  orderIndex: PropTypes.func.isRequired
+  orderIndex: PropTypes.func.isRequired,
+  history: PropTypes.shape().isRequired
+
 }
 
 const mapStateToProps = (state) => ({
