f66018771c47b842fe0f15e92a32e8802b5c5dd3
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-10 17:58:19 +0800

    admin

--- src/components/CartTable.jsx
+++ src/components/CartTable.jsx
@@ -1,141 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
 import Table from 'react-bootstrap/Table'
 import Image from 'react-bootstrap/Image'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { getMyCart } from '@/actions/my/cart/index'
 
-class CartTable extends React.Component {
-  constructor(props) {
-    super(props)
+const CartTable = ({ myCartState: { cart, isLoading }, ...props }) => {
+  useEffect(() => {
+    props.getMyCart()
+  }, [])
 
-    this.state = {
+  const [size, setSize] = useState(1)
+  const [quantity, setQuantity] = useState(1)
+  const [CartNextStep, setCartNextStep] = useState(null)
 
-    }
-  }
+  // CartNextStep() => {
+  //   const { history: { push } } = props
+  //   push(`/products?catName=${catId}`)
+  // }
 
-  render() {
-    return (
-      <div id="ComponentsCartTable" className="container">
+  // useEffect is componentDidMount and componentWillUpdate. Keep the [] as that is for componentWillUpdate.
+  // Template for useEffect
 
-        <Table className="text-center mt-3" responsive="md">
-          <thead>
-            <tr>
-              <th colSpan="2" />
-              <th>Unit Cost</th>
-              <th>Quantity</th>
-              <th>Subtotal</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
+  return (
+    <div id="ComponentsCartTable" className="container">
 
-            {/* Map this section */}
-            <tr>
-              <td>
-                <Image src="https://i.pinimg.com/736x/4a/91/a6/4a91a6b4f09c7df99a6352db06f7a701.jpg" className="pic-resize" />
-              </td>
-              <td>ProductName</td>
-              <td>$700</td>
-              <td>
-                <Form>
-                  <Form.Control as="select" aria-label="quantity" name="quantity">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                  </Form.Control>
-                </Form>
-              </td>
-              <td>Subtotal</td>
-              <td>
-                <div className="fas fa-trash-alt trashBtn"> Remove</div>
-              </td>
-            </tr>
+      <Table className="text-center mt-3" responsive="md">
+        <thead>
+          <tr>
+            <th colSpan="2" />
+            <th>Unit Cost</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {
+            cart.map((item) => (
+              <tr>
+                <td>
+                  <Image src={item.Product.Images?.[0]?.imageURL} className="pic-resize" />
+                </td>
+                <td>{item.Product.productName}</td>
+                <td>{item.Product.price}</td>
+                <td>
+                  <Form>
+                    <Form.Control as="select" aria-label="quantity" name="quantity">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </Form.Control>
+                  </Form>
+                </td>
+                <td>Subtotal</td>
+                <td>
+                  <div className="fas fa-trash-alt trashBtn"> Remove</div>
+                </td>
+              </tr>
+            ))
+          }
 
-          </tbody>
+        </tbody>
 
-          <tbody>
-            <tr>
-              <td colSpan="3" />
-              <td>
-                <h4>Total:</h4>
-              </td>
-              <td>
-                <h4>$100.00</h4>
-              </td>
-              <td>
-                <Button variant="success"> Next Step </Button>
-              </td>
-            </tr>
-          </tbody>
+        <tbody>
+          <tr>
+            <td colSpan="3" />
+            <td>
+              <h4>Total:</h4>
+            </td>
+            <td>
+              <h4>$100.00</h4>
+            </td>
+            <td>
+              <Button variant="success" onClick={() => CartNextStep()}> Next Step </Button>
+            </td>
+          </tr>
+        </tbody>
 
-        </Table>
-      </div>
-
-    )
-  }
+      </Table>
+    </div>
+  )
 }
 
-export default CartTable
+CartTable.propTypes = {
+  myCartState: PropTypes.shape().isRequired,
+  getMyCart: PropTypes.func.isRequired
 
-// Grid version
-// <div id="ComponentsCartTable" className="container">
+}
 
-//   <Container className="mt-3 text-center">
-//     <Row>
-//       <Col />
-//       <Col />
-//       <Col>
-//         <h4>Unit Cost</h4>
-//       </Col>
-//       <Col>
-//         <h4>Quantity</h4>
-//       </Col>
-//       <Col>
-//         <h4>Subtotal</h4>
-//       </Col>
-//       <Col />
-//     </Row>
-//     <Row className="d-flex align-items-center my-2">
-//       <Col>
-//         <Image src="https://i.pinimg.com/736x/4a/91/a6/4a91a6b4f09c7df99a6352db06f7a701.jpg" className="pic-resize" />
-//       </Col>
-//       <Col>ProductName</Col>
-//       <Col>$700</Col>
-//       <Col>
-//         <Form>
-//           <Form.Control as="select" aria-label="quantity" name="quantity">
-//             <option value="1">1</option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </Form.Control>
-//         </Form>
-//       </Col>
-//       <Col>Subtotal</Col>
-//       <Col>
-//         <div className="fas fa-trash-alt trashBtn"> Remove</div>
-//       </Col>
-//     </Row>
-//     <Row>
-//       <Col />
-//       <Col />
-//       <Col />
-//       <Col>
-//         <h4>Total:</h4>
-//       </Col>
-//       <Col>
-//         <h4>$100.00</h4>
-//       </Col>
-//       <Col />
+const mapStateToProps = (state) => ({
+  myCartState: state.myCart
+})
 
-//     </Row>
-//   </Container>
+const mapDispatchToProps = {
+  getMyCart
+}
 
-// </div>
+export default connect(mapStateToProps, mapDispatchToProps)(CartTable)
--- src/reducers/admin/admin-order-index.jsx
+++ src/reducers/admin/admin-order-index.jsx
@@ -31,8 +31,8 @@ export default (state = initialState, action) => {
         const index = draft.listAdminOrder.findIndex((order) => order.id === action.payload.order.id)
         if (index !== -1) draft.listAdminOrder[index] = action.payload.order
 
-        // draft[0] = action.payload.order
-        // if only one, then the singular order
+        // the above looks at listAdminOrder and finds the order that matches the id and updates it
+        // if only one order, then order should be singular, as is this case here since we are looking for one order
         // draft makes a copy of the state and compares between the two, then updates the change accordingly
       })
     }
