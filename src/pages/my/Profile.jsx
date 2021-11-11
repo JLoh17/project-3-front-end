0a9f5b4bbc76beb08a9249ed59908acbd5d87bc6
gitjonathan <yootwodeetwo@gmail.com>
2021-11-11 19:08:14 +0800

    edited profile

--- src/pages/my/Profile.jsx
+++ src/pages/my/Profile.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const MyProfile = () => (
   <div id="my-profile" className="container">
     <header className="text-center border-bottom">
       <h1>My Profile</h1>
-      <div><Link to="/">Home Page</Link></div>
+      {/* <div><Link to="/">Home Page</Link></div>
       <div><Link to="/my/Cart">My Cart</Link></div>
       <div><Link to="/my/orders">My Orders Index</Link></div>
-      <div><Link to="/admin/orders">My Admin Orders</Link></div>
+      <div><Link to="/admin/orders">My Admin Orders</Link></div> */}
     </header>
     <body>
       <div className="container">
